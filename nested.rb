a9c47aa74a22632c69b76fe5be09e24e1b2e7c22
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 19:35:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
         }
      }
 
-	value_hash = programmer_hash[:dennis_ritchie][:languages]
+	value_hash = programmer_hash[:dennis_ritchie][:languages]["C"]
 	return value_hash
 end
 
